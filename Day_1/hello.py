0856dbe3369af03abf5d9890c96fca0ef9b6bb6d
rodan0818 <rodanramdam0818@gmail.com>
2020-06-03 22:29:36 +0545

    Day 2 Complete

--- Day_1/hello.py
+++ Day_1/hello.py
@@ -8,3 +8,19 @@ print ('What is your age ?')#Asks for the age
 age = input()#takes age input
 print('Your age will be '+str((int(age)+1))+' after one year')
 
+"""
+I learned nothing new in this day but I reviewed the things I know already.
+
+Things done in this day
+Asked for Input From Used
+Used the console out commands i.e print
+Done string concatenation in line no. 9 
+Made single line comment using # as seen in the line no. 1,3,7,8
+[Note:
+While doing string concatenation I got error that int_type and string_type
+cannot be concatenated so I converted the integer using str() function which
+is prebuilt within python example can be seen in my code line no. 9 
+code part :'+str((int(age)+1))'
+]
+"""
+
